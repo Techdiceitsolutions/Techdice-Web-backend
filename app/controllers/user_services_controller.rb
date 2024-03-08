85c69d17f1339a0385dd14e0b414e710e351ed58
Daniel Ochuba <danielochuba78@gmail.com>
2024-03-08 14:12:46 +0100

    generate service and user_service relationships

---
+++ app/controllers/user_services_controller.rb
@@ -0,0 +1,51 @@
+class UserServicesController < ApplicationController
+  before_action :set_user_service, only: %i[ show update destroy ]
+
+  # GET /user_services
+  def index
+    @user_services = UserService.all
+
+    render json: @user_services
+  end
+
+  # GET /user_services/1
+  def show
+    render json: @user_service
+  end
+
+  # POST /user_services
+  def create
+    @user_service = UserService.new(user_service_params)
+
+    if @user_service.save
+      render json: @user_service, status: :created, location: @user_service
+    else
+      render json: @user_service.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /user_services/1
+  def update
+    if @user_service.update(user_service_params)
+      render json: @user_service
+    else
+      render json: @user_service.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /user_services/1
+  def destroy
+    @user_service.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_user_service
+      @user_service = UserService.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def user_service_params
+      params.require(:user_service).permit(:career_goals, :unique_qualities, :position, :resume, :exciting_project, :user_id, :service_id)
+    end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
 
+  has_many :user_service
+
   def jwt_payload
     super
   end
