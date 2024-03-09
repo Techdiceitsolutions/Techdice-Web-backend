122891b0bd5840c60125fd2b1615978ee08840c0
Daniel Ochuba <danielochuba78@gmail.com>
2024-03-09 15:49:15 +0100

    Add module to controllers

--- app/controllers/api/v1/services_controller.rb
+++ app/controllers/api/v1/services_controller.rb
@@ -1,52 +1,56 @@
-class ServicesController < ApplicationController
-  before_action :set_service, only: %i[show update destroy]
+module Api
+  module V1
+    class Api::V1::ServicesController < ApplicationController
+      before_action :set_service, only: %i[show update destroy]
 
-  # GET /services
-  def index
-    @services = Service.all
+      # GET /services
+      def index
+        @services = Service.all
 
-    render json: @services
-  end
+        render json: @services
+      end
 
-  # GET /services/1
-  def show
-    render json: @service
-  end
+      # GET /services/1
+      def show
+        render json: @service
+      end
 
-  # POST /services
-  def create
-    @service = Service.new(service_params)
+      # POST /services
+      def create
+        @service = Service.new(service_params)
 
-    if @service.save
-      render json: @service, status: :created, location: @service
-    else
-      render json: @service.errors, status: :unprocessable_entity
+        if @service.save
+          render json: @service, status: :created, location: @service
+        else
+          render json: @service.errors, status: :unprocessable_entity
+        end
+      end
+
+      # PATCH/PUT /services/1
+      def update
+        if @service.update(service_params)
+          render json: @service
+        else
+          render json: @service.errors, status: :unprocessable_entity
+        end
+      end
+
+      # DELETE /services/1
+      def destroy
+        @service.destroy
+      end
+
+      private
+
+      # Use callbacks to share common setup or constraints between actions.
+      def set_service
+        @service = Service.find(params[:id])
+      end
+
+      # Only allow a list of trusted parameters through.
+      def service_params
+        params.require(:service).permit(:name, :description, :icon, :duration, :vacancy)
+      end
     end
-  end
-
-  # PATCH/PUT /services/1
-  def update
-    if @service.update(service_params)
-      render json: @service
-    else
-      render json: @service.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /services/1
-  def destroy
-    @service.destroy
-  end
-
-  private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_service
-    @service = Service.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def service_params
-    params.require(:service).permit(:name, :description, :icon, :duration, :vacancy)
   end
 end
