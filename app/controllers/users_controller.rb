0976ffa1efe0da8d0941bfdba2fbc6131e82226d
Daniel Ochuba <danielochuba78@gmail.com>
2024-04-03 11:09:51 +0100

    remove redundant codes

--- app/controllers/users_controller.rb
+++
@@ -1,9 +0,0 @@
-class UsersController < ApplicationController
-  def index
-    if current_admin
-      render json: User.all
-    else
-      render json: { error: 'Unauthorized' }, status: :unauthorized
-    end
-  end
-end
--- db/migrate/20240308095134_add_course_specialization_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddCourseSpecializationToUser < ActiveRecord::Migration[7.0]
-  def change
-    add_column :users, :course_specialization, :string
-  end
-end
